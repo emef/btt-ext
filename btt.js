a7fc89f0b9582aca452d2c53775b8930527bd331
Matt Forbes <mforbes@twitter.com>
2017-09-18 16:48:04 -0700

    modal progress

--- btt.js
+++ btt.js
@@ -1,6 +1,39 @@
-var _log = null;
-var _overlay = null;
-var _modal = null;
+var _log;
+var _modal, _modalContainer, _modalOverlay, _modalBody, _modalFooter;
+
+$(document).ready(function() {
+  _modalOverlay = $('<div class="modal-overlay" style="z-index: 4001;" />');
+  _modal = $('<div class="modal-container" />');
+  var modalContent = $('<div class="modal-content" />');
+  _modalContainer = $('<div class="modal" />');
+  _modal.append(_modalContainer);
+  _modalContainer.append(modalContent);
+
+  _modal.click(hideModal);
+  _modal.find("*").click(function(e) { e.stopPropagation(); });
+
+  var modalHeader = $('<div class="modal-header">' +
+                 '<h3 class="modal-title">Buy this tweet</h3>' +
+                 '</div>');
+  modalContent.append(modalHeader);
+
+  _modalBody = $('<div class="modal-body modal-tweet btt-body" />');
+  modalContent.append(_modalBody);
+
+  _modalFooter = $('<div class="modal-tweet-form-container btt-footer" />');
+  modalContent.append(_modalFooter);
+
+  var modalClose = $('<button type="button" class="modal-btn modal-close js-close">' +
+                     '<span class="Icon Icon--close Icon--medium" />' +
+                     '</button>');
+  modalClose.click(hideModal);
+  modalContent.append(modalClose);
+
+  hideModal();
+
+  $(document.body).append(_modalOverlay);
+  $(document.body).append(_modal);
+});
 
 function log() {
   if (!_log) {
@@ -13,20 +46,40 @@ function log() {
   _log.apply(this, arguments);
 }
 
-function modal() {
-  if (!_modal) {
-    _modal = $('<div />');
-  }
+function showModal() {
+  var leftPx = ($(document).width() - _modalContainer.width()) / 2;
+  _modalContainer.css("left", leftPx + "px");
 
-  if (!overlay) {
-    _overlay = $('<div class="modal-overlay" style="z-index: 4001;" />');
-  }
+  _modalOverlay.fadeIn(200);
+  _modal.fadeIn(200);
+  $(document).bind('keyup', escHandler);
+}
 
-  return _modal;
+function hideModal() {
+  _modalOverlay.hide();
+  _modal.hide();
+  _modalBody.find("*").remove();
+  $(document).unbind('keyup', escHandler);
+}
+
+function escHandler(e) {
+  if (e.keyCode === 27) {
+    hideModal();
+  }
 }
 
 function onClickBuyThisTweet(tweetId) {
   log('clicked', tweetId);
+  showModal();
+
+  // TODO: dynamic image from tweetId
+  _modalBody.append($('<img src="https://oo-prod.s3.amazonaws.com/public/mockups/59c050d2eed6400a238d694e.png" />'));
+
+  // TODO: dynamic tweet author
+  var handle = 'Interior';
+  _modalFooter.append($('<div class="btt-user">Buy a tweet from ' +
+                        '<span class="btt-handle">@' + handle + '</span>' +
+                        '</div>'));
 
 }
 
@@ -56,6 +109,5 @@ setInterval(function() {
     bttBtn.append(bttIconContainer);
     bttIconContainer.append(bttIcon);
     $(actionList).append(bttDiv);
-
   });
 }, 50);
